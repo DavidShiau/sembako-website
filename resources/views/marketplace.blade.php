d7016c457225049259d054d96b74c8a38ae7f249
fianfr <viiandhardcore@gmail.com>
2020-07-11 12:38:27 +0700

    Sampai halaman Toko

--- resources/views/marketplace.blade.php
+++ resources/views/marketplace.blade.php
@@ -20,43 +20,61 @@
         <!-- Content Area -->
         <section>
             <div class="row marketplace-content">
-                <div class="col-lg-5">
+                <div class="col-lg-3">
                     <img src="{{ asset('/img/warung.jpg') }}"  alt="content" class="img-fluid">
+                    <button class="btn btn-primary btn-follow" name="btn-follow" type="submit">Ikuti</button>
                 </div>
 
-                <div class="col-lg-6">
+                <div class="col-lg-8">
                     <h2 class="page-title">Toko Bu Tina</h2>
+                    <div class="marketplace-rating">
+							<span class="fa fa-star"></span>
+							<span class="fa fa-star"></span>
+							<span class="fa fa-star"></span>
+                            <span class="fa fa-star"></span>
+                            <span class="ti-star"></span>
+                    </div>
                 
 
-                        <div class="product-info-more">
-                            <div class="product-value">
+                        <div class="marketplace-info-more">
+                            <div class="marketplace-value">
+                                <i class="ti-location-pin"></i>
                                 <span class="info-title">Lokasi</span>
                                 <span class="info-value">
                                 SKT, Surakarta
                                 </span>
                             </div>
 
-                            <div class="product-value">
-                                <span class="info-title">Distributor</span>
+                            <div class="marketplace-value">
+                                <i class="ti-mobile"></i>
+                                <span class="info-title">Telp.</span>
                                 <span class="info-value">
-                                <a target="_blank" href="/dashboard">
-                                Toko Bu Tina    </a>
+                                    081772328445
+                                </span>
+                            </div>
+
+                            <div class="marketplace-value">
+                                <i class="ti-time"></i>
+                                <span class="info-title">Bergabung</span>
+                                <span class="info-value">
+                                    11 Bulan Lalu
+                                </span>
+                            </div>
+
+                            <div class="marketplace-value">
+                                <i class="ti-user"></i>
+                                <span class="info-title">Pengikut</span>
+                                <span class="info-value">
+                                    286
                                 </span>
                             </div>
                         </div>
 
-                        <div class="product-info-description">
-                            FORTUNE Beras Premium 5kg merupakan beras persembahan SANIA yang dihasilkan dari padi pilihan dengan olahan terbaik. Beras berkualitas tinggi bersih dan sangat enak. Beras ini tanpa menggunakan pemutih sehingga aman di konsumsi keluarga Anda setiap hari.
+                        <div class="marketplace-info-description">
+                            <span class="info-title">Deskripsi Toko</span>
+                            <p>Kami buka setiap hari, dari jam 07.00 - 16.00 WIB.</p>
                         </div>
 
-                        <div class="product-info-price">
-                            Rp65.000
-                        </div>
-
-                        <button class="btn btn-primary btn-input-bag" name="btn-input-bag" type="submit">Masukkan Keranjang</button>
-                        <button class="btn btn-primary btn-favourite" name="btn-favourite" type="favourite">
-                            <i class="ti-heart"></i>
-                        </button>
                     
                 </div>
             </div>
@@ -64,7 +82,179 @@
                 
         <!-- End Content Area -->
 
+        <!-- Main Product Area -->
+
+        <div class="product-area">
+            <h4>Produk Toko</h4>
+
+            <div class="row product-card">
+                <div class="column">
+                    <div class="card-item">
+                        <img src="/img/beras.png" alt="Beras" style="width:100%">
+                        <div class="card-info">
+                            <span class="card-title">Beras Putih Fortune</span>
+                            <p class="price">Rp65.000</p>
+                            <a href="/product"  style="width:100%">
+                                <span class="btn btn-primary btn-add">
+                                    <i class="fa fa-shopping-cart"></i>
+                                </span>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                
+                <div class="column">
+                    <div class="card-item">
+                        <img src="/img/beras.png" alt="Beras" style="width:100%">
+                        <div class="card-info">
+                            <span class="card-title">Beras Putih Fortune</span>
+                            <p class="price">Rp65.000</p>
+                            <a href="/product"  style="width:100%">
+                                <span class="btn btn-primary btn-add">
+                                    <i class="fa fa-shopping-cart"></i>
+                                </span>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="column">
+                    <div class="card-item">
+                        <img src="/img/beras.png" alt="Beras" style="width:100%">
+                        <div class="card-info">
+                            <span class="card-title">Beras Putih Fortune</span>
+                            <p class="price">Rp65.000</p>
+                            <a href="/product"  style="width:100%">
+                                <span class="btn btn-primary btn-add">
+                                    <i class="fa fa-shopping-cart"></i>
+                                </span>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                
+                <div class="column">
+                    <div class="card-item">
+                        <img src="/img/beras.png" alt="Beras" style="width:100%">
+                        <div class="card-info">
+                            <span class="card-title">Beras Putih Fortune</span>
+                            <p class="price">Rp65.000</p>
+                            <a href="/product"  style="width:100%">
+                                <span class="btn btn-primary btn-add">
+                                    <i class="fa fa-shopping-cart"></i>
+                                </span>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="column">
+                    <div class="card-item">
+                        <img src="/img/beras.png" alt="Beras" style="width:100%">
+                        <div class="card-info">
+                            <span class="card-title">Beras Putih Fortune</span>
+                            <p class="price">Rp65.000</p>
+                            <a href="/product"  style="width:100%">
+                                <span class="btn btn-primary btn-add">
+                                    <i class="fa fa-shopping-cart"></i>
+                                </span>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+
+
+            </div>
+            
+            <div class="row product-card">
+                <div class="column">
+                    <div class="card-item">
+                        <img src="/img/beras.png" alt="Beras" style="width:100%">
+                        <div class="card-info">
+                            <span class="card-title">Beras Putih Fortune</span>
+                            <p class="price">Rp65.000</p>
+                            <a href="/product"  style="width:100%">
+                                <span class="btn btn-primary btn-add">
+                                    <i class="fa fa-shopping-cart"></i>
+                                </span>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                
+                <div class="column">
+                    <div class="card-item">
+                        <img src="/img/beras.png" alt="Beras" style="width:100%">
+                        <div class="card-info">
+                            <span class="card-title">Beras Putih Fortune</span>
+                            <p class="price">Rp65.000</p>
+                            <a href="/product"  style="width:100%">
+                                <span class="btn btn-primary btn-add">
+                                    <i class="fa fa-shopping-cart"></i>
+                                </span>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="column">
+                    <div class="card-item">
+                        <img src="/img/beras.png" alt="Beras" style="width:100%">
+                        <div class="card-info">
+                            <span class="card-title">Beras Putih Fortune</span>
+                            <p class="price">Rp65.000</p>
+                            <a href="/product"  style="width:100%">
+                                <span class="btn btn-primary btn-add">
+                                    <i class="fa fa-shopping-cart"></i>
+                                </span>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                
+                <div class="column">
+                    <div class="card-item">
+                        <img src="/img/beras.png" alt="Beras" style="width:100%">
+                        <div class="card-info">
+                            <span class="card-title">Beras Putih Fortune</span>
+                            <p class="price">Rp65.000</p>
+                            <a href="/product"  style="width:100%">
+                                <span class="btn btn-primary btn-add">
+                                    <i class="fa fa-shopping-cart"></i>
+                                </span>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="column">
+                    <div class="card-item">
+                        <img src="/img/beras.png" alt="Beras" style="width:100%">
+                        <div class="card-info">
+                            <span class="card-title">Beras Putih Fortune</span>
+                            <p class="price">Rp65.000</p>
+                            <a href="/product"  style="width:100%">
+                                <span class="btn btn-primary btn-add">
+                                    <i class="fa fa-shopping-cart"></i>
+                                </span>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            
+            </div>
+
+            <a href="/dashboard" class="act-btn">
+                <span class="fa fa-cart-plus" alt="" title="Cek Keranjang Belanja Kamu!"></span>
+            </a>
+
+        </div>
+
+        <!-- end Main Product Area -->
+
     </div>
+
+    
 
     @include('layout/footer')
 
